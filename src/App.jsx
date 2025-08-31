a4a058de4f0bb2f29972e5775ce045c51b3116a1
Harsh Shukla <you@example.com>
2025-08-31 18:27:33 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Hero from './components/custom/Hero'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   const [count, setCount] = useState(0)
 
